cc3669063ec76816856ebe70170c6c5e3607e0d9
MalavikaSreehari <malavikasreehari4@gmail.com>
2024-02-21 10:07:43 +0530

    feat: connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quikhyr/app.dart';
 import 'package:quikhyr/simple_bloc_observer.dart';
 import 'package:user_repository/user_repository.dart';
+import 'firebase_options.dart';
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
 }
