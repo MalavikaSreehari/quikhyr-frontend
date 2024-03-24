97f764cd2e43b063a86c68fbeb2f4c264b82038f
MalavikaSreehari <malavikasreehari4@gmail.com>
2024-03-24 21:52:47 +0530

    feat: basic settings_screen ui

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:quikhyr/common/constants/app_asset_links.dart';
+import 'package:quikhyr/common/constants/app_colors.dart';
 import 'package:quikhyr/common/constants/app_sizing.dart';
 import 'package:quikhyr/common/widgets/clickable_svg_icon.dart';
+import 'package:quikhyr/common/widgets/gradient_separator.dart';
+import 'package:quikhyr/common/widgets/quik_search_bar.dart';
 
 class SettingsScreen extends StatelessWidget {
   SettingsScreen({Key? key}) : super(key: key);
@@ -59,6 +62,110 @@ class SettingsScreen extends StatelessWidget {
                     // context.read<SignInBloc>().add(const SignOutRequired());
                   }),
               AppSizing.hS24(),
+            ],
+          ),
+    
+        ),
+      ),
+      body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [ QuikSearchBar(onChanged: (String onChanged) {}, hintText: 'Search for services..', onMicPressed: (){}, onSearch: (String value){}, controller: TextEditingController()),
+            const SizedBox(height: 24,),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('PROFILE',style: Theme.of(context).textTheme.titleMedium,)),
+              const SizedBox(height: 24,),
+              const Row(
+                children: [
+                  CircleAvatar(radius: 32,),
+                  SizedBox(width: 16,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Noah Johny',style: TextStyle(
+  color: Color(0xFFFAFFFF), 
+  fontFamily: 'Trap', 
+  fontSize: 20, 
+  fontStyle: FontStyle.normal, 
+  fontWeight: FontWeight.w700, 
+  height: 1.0, 
+),),
+SizedBox(height: 12,),
+                      Text('MEMBER',
+                      style: TextStyle(
+  color: Color(0xFF0039CC), 
+  fontFamily: 'Trap', 
+  fontSize: 14, 
+  fontStyle: FontStyle.normal,
+  fontWeight: FontWeight.w500, 
+  height: 1.0, 
+),),],),
+Text('Edit')
+                ],
+              ),
+              const SizedBox(height: 24,),
+
+              const GradientSeparator(),
+              const SizedBox(height: 24,),
+              Text('SETTINGS',style: Theme.of(context).textTheme.titleMedium,),
+              const SizedBox(height: 24,),
+              ListView(
+                shrinkWrap: true,
+                
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.person, color: labelColor,),
+                    title: Text('Manage Account',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.mark_unread_chat_alt, color: labelColor,),
+                    title: Text('Manage Notifications',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.block_rounded, color: labelColor,),
+                    title: Text('Block/Unblock Workers',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  
+                ],
+              ),
+              Text('SUPPORT',style: Theme.of(context).textTheme.titleMedium,),
+              const SizedBox(height: 24,),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.help, color: labelColor,),
+                    title: Text('Explore FAQs',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.bug_report_rounded, color: labelColor,),
+                    title: Text('Report Bugs',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  
+                ],
+              ),
+               Text('ABOUT',style: Theme.of(context).textTheme.titleMedium,),
+               
+              const SizedBox(height: 24,),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.help, color: labelColor,),
+                    title: Text('Terms and Conditions',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.star, color: labelColor,),
+                    title: Text('Report Bugs',style: Theme.of(context).textTheme.bodyMedium,),
+                  ),
+                  
+                ],
+              )
+              
             ],
           ),
         ),
