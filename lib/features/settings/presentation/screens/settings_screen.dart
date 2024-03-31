a15bca8d5bb7f2443f8eb05aa058d729d25f98a7
navaneethku <navaneethku1729@gmail.com>
2024-03-31 12:50:31 +0530

    fix: settings appbar ui, need to test for other screen sizes

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -8,68 +8,74 @@ import 'package:quikhyr/common/widgets/gradient_separator.dart';
 import 'package:quikhyr/common/widgets/quik_search_bar.dart';
 
 class SettingsScreen extends StatelessWidget {
-  SettingsScreen({Key? key}) : super(key: key);
+  const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        
-        preferredSize: const Size.fromHeight(329),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 12),
-          child: AppBar(
-            toolbarHeight: 329,
-            titleSpacing: 24,
-            automaticallyImplyLeading: false, // Remove back button
-            backgroundColor: Colors.transparent,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RichText(
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Q',
-                        style: TextStyle(
-                            fontFamily: 'Moonhouse', fontSize: 32),
-                      ),
-                      TextSpan(
-                        text: 'uik',
-                        style: TextStyle(
-                            fontFamily: 'Moonhouse', fontSize: 24),
-                      ),
-                      TextSpan(
-                        text: 'Settings',
-                        style: TextStyle(
-                            fontFamily: 'Trap',
-                            fontSize: 24,
-                            letterSpacing: -1.5),
-                      ),
-                    ],
-                  ),
+      appBar: AppBar(
+        toolbarHeight: 337,
+        titleSpacing: 24,
+        automaticallyImplyLeading: false, // Remove back button
+        backgroundColor: Colors.transparent,
+        title: Container(
+          padding: const EdgeInsets.only(top: 18),
+          height: 337,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Q',
+                      style: TextStyle(fontFamily: 'Moonhouse', fontSize: 32),
+                    ),
+                    TextSpan(
+                      text: 'uik',
+                      style: TextStyle(fontFamily: 'Moonhouse', fontSize: 24),
+                    ),
+                    TextSpan(
+                      text: 'Settings',
+                      style: TextStyle(
+                          fontFamily: 'Trap',
+                          fontSize: 24,
+                          letterSpacing: -1.5),
+                    ),
+                  ],
                 ),
-                AppSizing.vS24(),
-                QuikSearchBar(
+              ),
+              AppSizing.vS30(),
+              QuikSearchBar(
                 onChanged: (String onChanged) {},
                 hintText: 'Search for settings..',
                 onMicPressed: () {},
                 onSearch: (String value) {},
                 controller: TextEditingController(),
               ),
-              const SizedBox(height: 24,),
+              const SizedBox(
+                height: 24,
+              ),
               Align(
                 alignment: Alignment.centerLeft,
-                child: Text('PROFILE',style: Theme.of(context).textTheme.titleMedium,),
+                child: Text(
+                  'PROFILE',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
               ),
-              const SizedBox(height: 24,),
+              const SizedBox(
+                height: 24,
+              ),
               Row(
                 children: [
                   const CircleAvatar(
                     radius: 32,
-                    backgroundImage: NetworkImage(AppAssetLinks.placeholderImage),
+                    backgroundImage:
+                        NetworkImage(AppAssetLinks.placeholderImage),
                   ),
-                  const SizedBox(width: 16,),
+                  const SizedBox(
+                    width: 16,
+                  ),
                   const Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -84,7 +90,9 @@ class SettingsScreen extends StatelessWidget {
                           height: 1.0,
                         ),
                       ),
-                      SizedBox(height: 12,),
+                      SizedBox(
+                        height: 12,
+                      ),
                       Text(
                         'MEMBER',
                         style: TextStyle(
@@ -98,38 +106,50 @@ class SettingsScreen extends StatelessWidget {
                       ),
                     ],
                   ),
-                  const SizedBox(width: 12,),
+                  const SizedBox(
+                    width: 12,
+                  ),
                   Align(
                     alignment: Alignment.bottomRight,
                     child: OutlinedButton.icon(
                       onPressed: () {},
-                      icon: const Icon(Icons.edit,color: secondary,),
-                      label: const Text('Edit',style: TextStyle(color: secondary,),),
-                      style: OutlinedButton.styleFrom(side: const BorderSide(color: secondary)),
+                      icon: const Icon(
+                        Icons.edit,
+                        color: secondary,
+                      ),
+                      label: const Text(
+                        'Edit',
+                        style: TextStyle(
+                          color: secondary,
+                        ),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                          side: const BorderSide(color: secondary)),
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 24,),
+              const SizedBox(
+                height: 24,
+              ),
               const GradientSeparator(),
-              ],
-            ),
-            // actions: [
-            //   ClickableSvgIcon(
-            //       svgAsset: AppAssetLinks.bellNotificationActiveSvg,
-            //       onTap: () {
-            //         //HANDLE GO TO NOTIFICATIONS
-            //       }),
-            //   AppSizing.hS10(),
-            //   ClickableSvgIcon(
-            //       svgAsset: AppAssetLinks.logoutSvg,
-            //       onTap: () {
-            //         // context.read<SignInBloc>().add(const SignOutRequired());
-            //       }),
-            //   AppSizing.hS24(),
-            // ],
+            ],
           ),
         ),
+        // actions: [
+        //   ClickableSvgIcon(
+        //       svgAsset: AppAssetLinks.bellNotificationActiveSvg,
+        //       onTap: () {
+        //         //HANDLE GO TO NOTIFICATIONS
+        //       }),
+        //   AppSizing.hS10(),
+        //   ClickableSvgIcon(
+        //       svgAsset: AppAssetLinks.logoutSvg,
+        //       onTap: () {
+        //         // context.read<SignInBloc>().add(const SignOutRequired());
+        //       }),
+        //   AppSizing.hS24(),
+        // ],
       ),
       body: SingleChildScrollView(
         physics: const ClampingScrollPhysics(),
@@ -138,59 +158,114 @@ class SettingsScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              
-              
-              Text('SETTINGS',style: Theme.of(context).textTheme.titleMedium,),
-              const SizedBox(height: 24,),
+              Text(
+                'SETTINGS',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               SingleChildScrollView(
                 physics: const NeverScrollableScrollPhysics(),
                 child: Column(
                   children: [
                     ListTile(
-                      leading: const Icon(Icons.person, color: labelColor,),
-                      title: Text('Manage Account',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.person,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Manage Account',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                     ListTile(
-                      leading: const Icon(Icons.mark_unread_chat_alt, color: labelColor,),
-                      title: Text('Manage Notifications',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.mark_unread_chat_alt,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Manage Notifications',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                     ListTile(
-                      leading: const Icon(Icons.block_rounded, color: labelColor,),
-                      title: Text('Block/Unblock Workers',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.block_rounded,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Block/Unblock Workers',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                   ],
                 ),
               ),
-              Text('SUPPORT',style: Theme.of(context).textTheme.titleMedium,),
-              const SizedBox(height: 24,),
+              Text(
+                'SUPPORT',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               SingleChildScrollView(
                 physics: const NeverScrollableScrollPhysics(),
                 child: Column(
                   children: [
                     ListTile(
-                      leading: const Icon(Icons.help, color: labelColor,),
-                      title: Text('Explore FAQs',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.help,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Explore FAQs',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                     ListTile(
-                      leading: const Icon(Icons.bug_report_rounded, color: labelColor,),
-                      title: Text('Report Bugs',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.bug_report_rounded,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Report Bugs',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                   ],
                 ),
               ),
-              Text('ABOUT',style: Theme.of(context).textTheme.titleMedium,),
-              const SizedBox(height: 24,),
+              Text(
+                'ABOUT',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
               SingleChildScrollView(
                 physics: const NeverScrollableScrollPhysics(),
                 child: Column(
                   children: [
                     ListTile(
-                      leading: const Icon(Icons.help, color: labelColor,),
-                      title: Text('Terms and Conditions',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.help,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Terms and Conditions',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                     ListTile(
-                      leading: const Icon(Icons.star, color: labelColor,),
-                      title: Text('Report Bugs',style: Theme.of(context).textTheme.bodyMedium,),
+                      leading: const Icon(
+                        Icons.star,
+                        color: labelColor,
+                      ),
+                      title: Text(
+                        'Report Bugs',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
                     ),
                   ],
                 ),
